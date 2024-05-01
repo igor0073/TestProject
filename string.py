89e47ec337a8909f6216de500eced5b8d8053bf0
igor0073 <166950497+igor0073@users.noreply.github.com>
2024-05-01 20:16:42 +0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,28 @@
+name='igor shvetsov '
+print(name)
+print('hello '+ name)
+print(name * 7)
+print(name[0])
+print(name[-2])
+print(name[:10])
+print(name[::-1])
+name=(str)
+str='igor shvetsov'
+print(len(str))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
